da678876f22631e701d1158c4f8f61ed81067076
Aroob Alhumaidy <aroob.alhumaidy@gmail.com>
2025-04-06 19:32:48 +0100

    fix

--- main.py
+++ main.py
@@ -75,6 +75,8 @@ def main():
         save_combined_xml(all_records, output_dir)
         save_combined_csv(all_records, output_dir)
         save_combined_ris(all_records, output_dir)
+        save_as_csv(keyword, records, output_dir)
+        save_as_ris(keyword, records, output_dir)
 
     generate_summary_report(all_records, output_dir)
     log_failures(output_dir, failure_log)
